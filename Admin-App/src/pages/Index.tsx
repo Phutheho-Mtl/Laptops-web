d3ae640ed351071e267e4eccbd81b7f0acaf904f
Phutheho Mtloung <Mtloungphutheho@gmail.com>
2026-01-09 17:55:57 +0200

    built the basic add photos component

--- Admin-App/src/pages/Index.tsx
+++ Admin-App/src/pages/Index.tsx
@@ -3,12 +3,14 @@ import FeaturedProducts from "@/components/FeaturedProducts";
 import ServicesSection from "@/components/ServicesSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
+import ImagesUploader from "@/components/ImageUploader";
 import WhatsAppButton from "@/components/WhatsAppButton";
 
 const Index = () => {
   return (
     <main className="min-h-screen bg-background">
       <HeroSection />
+      <ImagesUploader/>
       <FeaturedProducts />
       <ServicesSection />
       <ContactSection />
